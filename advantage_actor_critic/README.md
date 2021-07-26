5bec139d227ba868ab395c31f1970c743e20f6c3
adi3e08 <adithya.ramesh.1993@gmail.com>
2021-07-26 12:30:40 +0530

    added advantage actor critic

--- advantage_actor_critic/README.md
+++ advantage_actor_critic/README.md
@@ -1,6 +1,26 @@
 # Advantage Actor Critic
 
-Single thread version of Asynchronous Advantage Actor Critic algorithm (A3C).
+Advantage Actor Critic algorithm - single thread version of A3C (Asynchronous Advantage Actor Critic) algorithm.
 
-References
-1) "Asynchronous Methods for Deep Reinforcement Learning", Mnih et al. [Link](https://arxiv.org/abs/1602.01783).
+## References
+* "Asynchronous Methods for Deep Reinforcement Learning", Mnih et al. [Link](https://arxiv.org/abs/1602.01783).
+
+## Results
+* [Cart Pole](https://gym.openai.com/envs/CartPole-v1/) - Move back and forth to balance a pole on a cart.
+
+<p align="center">
+Cumulative Reward (total reward collected during episode) vs Episode, during training.
+</p>
+<p align="center">
+<img src="media/cartpole_train.png" width="75%" height="75%"/>
+</p>
+
+<p align="center">
+Evaluate agent performance every 1000 episodes. Each time calculate average cumulative reward over 100 episodes.
+</p>
+<p align="center"> 
+<img src="media/cartpole_eval.png" width="75%" height="75%"/>
+</p>
+
+
+
--- advantage_actor_critic/advantage_actor_critic.py
+++ advantage_actor_critic/advantage_actor_critic.py
@@ -80,6 +80,7 @@ class advantage_actor_critic:
                 R.append(r)
                 o = o_1
                 if (t % self.arglist.update_every == 0 or done):
+                # if done:
                     if done:
                         v_s_ = 0.0 # terminal
                     else:
@@ -94,10 +95,11 @@ class advantage_actor_critic:
                     v_targets.reverse()
                     v_targets = torch.tensor(v_targets, dtype=torch.float, device=self.device)
                     td = v_targets - values
-                    a_loss = - (log_probs * td.detach() + 0.01 * entrops)                                 
-                    c_loss = td.pow(2)
+                    a_loss = - (log_probs * td.detach()).mean()
+                    e_loss = - entrops.mean()                                
+                    c_loss = td.pow(2).mean()
 
-                    loss = (a_loss + 0.5 * c_loss).sum()
+                    loss = (a_loss + 0.01 * e_loss + 0.5 * c_loss)
                     self.optimizer.zero_grad()
                     loss.backward()
                     # torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.arglist.clip_term)
@@ -133,24 +135,24 @@ class advantage_actor_critic:
 
 def parse_args():
     parser = argparse.ArgumentParser("Advantage Actor Critic")
-    parser.add_argument("--exp-name", type=str, default="expt_1", help="name of experiment")
-    parser.add_argument("--episodes", type=int, default=50000, help="number of episodes")
+    parser.add_argument("--exp-name", type=str, default="expt_cartpole", help="name of experiment")
+    parser.add_argument("--episodes", type=int, default=25000, help="number of episodes")
     # Core training parameters
     parser.add_argument("--lr", type=float, default=2.5e-4, help="learning rate")
     parser.add_argument("--gamma", type=float, default=0.99, help="discount factor")
     # parser.add_argument("--clip-term", type=float, default=0.5, help="gradient clipping parameter")
-    parser.add_argument("--update-every", type=int, default=4, help="train after every _ steps")
+    parser.add_argument("--update-every", type=int, default=5, help="train after every _ steps")
     parser.add_argument("--eval-every", type=int, default=1000, help="eval every _ episodes")
     parser.add_argument("--eval-over", type=int, default=100, help="eval over _ episodes")
     return parser.parse_args()
 
 def make_env():
-    # env = gym.make('CartPole-v1')
-    # env.state_size = 4
-    # env.action_size = 2
-    env = gym.make('MountainCar-v0')
-    env.state_size = 2
-    env.action_size = 3
+    env = gym.make('CartPole-v1')
+    env.state_size = 4
+    env.action_size = 2
+    # env = gym.make('MountainCar-v0')
+    # env.state_size = 2
+    # env.action_size = 3
     return env
 
 if __name__ == '__main__':
